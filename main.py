df7a33ef74b7aa441b6d174c97b0cbddc82f9b10
Daniel Lugo <58088181+dalugoSU@users.noreply.github.com>
2021-02-23 21:00:26 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+from collection import begin_collection
+
+if __name__ == "__main__":
+  begin_collection() # Paste Your Webhook inside function, in quotes, if you would like to use the same one everytime
