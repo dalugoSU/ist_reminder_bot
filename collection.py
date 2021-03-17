88ae856f644964b3f195a2d3e503906c92d6dc56
Daniel Lugo <dalugo@syr.edu>
2021-03-16 23:13:43 -0400

    New File for logic. Fixed bug that would not end loop when quitting

--- collection.py
+++ collection.py
@@ -4,7 +4,7 @@ class AssignmentCollector:
     import requests
     import pymsteams
 
-    def get_class_assignments(self):
+    def get_class_assignments(self) -> list:
         """
         Gets table from IST Website and creates a 2D list array of due material.
         :return: list - 2D array. [[dueDate[0], dueTime[1], gradeBook[2], points[3], tool[4], whatsDue[5]]]
@@ -13,15 +13,17 @@ class AssignmentCollector:
         response = self.requests.get("http://ist256.com/syllabus/#course-schedule")  # Pulls website
         soup = self.BeautifulSoup(response.content, 'html.parser')  # Parses website and extracts its contents
 
-        assignments_table = soup.find(text='Date Due').find_parent('table')  # Finds the due date table by matching its text with table tag
+        assignments_table = soup.find(text='Date Due').find_parent(
+            'table')  # Finds the due date table by matching its text with table tag
         assignments = []  # List that will hold lists of assignments
 
         for table_row in assignments_table.find_all("tr")[1:]:
-            assignments.append([text.get_text(strip=True) for text in table_row.find_all("td")])  # Create a list for each row
+            assignments.append(
+                [text.get_text(strip=True) for text in table_row.find_all("td")])  # Create a list for each row
 
         return assignments  # Return 2D array of assignments and due dates
 
-    def get_date(self):
+    def get_date(self) -> str:
         """
         Function that gets today's date
         :return: return a string with date in format m/d/y
@@ -32,7 +34,7 @@ class AssignmentCollector:
 
         return date
 
-    def get_assignment(self):
+    def get_assignment(self) -> list:
         """
         Get the assignment due today if there is any
         :return: list of today's assignments.
@@ -53,7 +55,7 @@ class AssignmentCollector:
         else:
             return today_assignment
 
-    def push_to_ms(self, webhook):
+    def push_to_ms(self, webhook: str):
         """
         function to push today's assignments to an MS teams Team.
         :return: Void/No returns
@@ -74,58 +76,3 @@ class AssignmentCollector:
         connection.send()
 
         print("Your Reminder Has Been Posted")
-
-
-def begin_collection(ms_webhook=None): # ms_webhook = None in case you do not have one saved in main.py
-    due_assignment = AssignmentCollector() # Create instance of Assignment Collector Class
-    user_choice = input("Check Today's Assignments [yes, no or quit to exit]: ")
-
-    while user_choice != 'quit':
-        if user_choice.lower() == "yes":
-            due_today = due_assignment.get_assignment() # Assignments due "today"
-            print("")
-            for item in range(0, len(due_today)):
-                print(due_today[item])
-            push = input("\nWould you like to push assignments to teams [yes or no]: ")
-            if push.lower() == 'yes':
-                valid = False
-                if ms_webhook:
-                    while not valid or ms_webhook.lower() == 'quit':
-                        try:
-                            due_assignment.push_to_ms(webhook=ms_webhook)
-                            valid = True
-                        except:
-                            print("Invalid Webhook!")
-                            break
-                else:
-                    while not valid or ms_webhook.lower() == 'quit':
-                        ms_webhook = input("Enter MS Teams webhook: ")
-                        try:
-                            due_assignment.push_to_ms(webhook=ms_webhook)
-                            valid = True
-                        except:
-                            print("Invalid Webhook!")
-                break
-            else:
-                print("See you later!")
-                break
-        elif user_choice == 'no':
-            print("See ya later!")
-            break
-        else:
-            push = input("Would you like to push assignments to teams [yes or no]: ")
-            if push.lower() == 'yes':
-                valid = False
-                while not valid:
-                    ms_webhook = input("Enter MS Teams webhook: ")
-                    if ms_webhook.lower() == "quit":
-                        break
-                    try:
-                        due_assignment.push_to_ms(webhook=ms_webhook)
-                        valid = True
-                    except:
-                        print("Invalid Webhook!")
-                break
-            else:
-                print("See you later!")
-                break
