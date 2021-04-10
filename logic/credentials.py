df64a960090a1243ff6c08c57bb8797e97f0d487
Daniel Lugo <dalugo@syr.edu>
2021-04-10 00:32:05 -0400

    updates

--- logic/credentials.py
+++ logic/credentials.py
@@ -1,3 +1,8 @@
+"""
+Add bot email
+Add bot password
+Add email recipients in the list separated by comma
+"""
 email_credentials = {
     'email_from': 'BOTEMAIL', # Place bot email here
     'password': 'BOT EMAIL PASSWORD', # Bot email password
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from logic.logic import begin_collection
 import logic.webhook as wh
 
 if __name__ == "__main__":
-    begin_collection(ms_webhook=wh.WEBHOOK)  # Go to webhook.py to save your webhook
+    begin_collection(ms_webhook=wh.WEBHOOK_NONE)  # Go to webhook.py to save your webhook
